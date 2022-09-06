0e263673b108cd493a16a4fd04e26006dc2dd3df
shvetsov-as <a002eshvetsov@gmail.com>
2022-09-06 23:41:29 +0300

    938

--- test/com/shvetsov/leetcode/rangesumofbst938/SolutionTest.java
+++ test/com/shvetsov/leetcode/rangesumofbst938/SolutionTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
     Solution solution;
+    Solution2 solution2;
     TreeNode treeNode1;
     TreeNode treeNode2;
 
     @BeforeEach
     void setUp() {
         solution = new Solution();
+        solution2 = new Solution2();
         treeNode1 = new TreeNode(10,
                         new TreeNode(5, new TreeNode(3), new TreeNode(7)),
                         new TreeNode(15, new TreeNode(), new TreeNode(18)));
@@ -29,5 +30,11 @@ class SolutionTest {
     void shouldReturnSumInGivenRange() {
         assertThat(solution.rangeSumBST(treeNode1, 7, 15)).isEqualTo(32);
         assertThat(solution.rangeSumBST(treeNode2, 6, 10)).isEqualTo(23);
+    }
+
+    @Test
+    void shouldReturnSumInGivenRange2() {
+        assertThat(solution2.rangeSumBST(treeNode1, 7, 15)).isEqualTo(32);
+        assertThat(solution2.rangeSumBST(treeNode2, 6, 10)).isEqualTo(23);
     }
 }
